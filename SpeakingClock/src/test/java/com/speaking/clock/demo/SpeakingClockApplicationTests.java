2f8f14a8e283b744cbd6879bbfffc6b00f12acc0
Santhosh Durgam <durgamsanthosh141@gmail.com>
2023-09-18 14:30:10 +0530

    Add files via upload

---
+++ SpeakingClock/src/test/java/com/speaking/clock/demo/SpeakingClockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speaking.clock.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakingClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
